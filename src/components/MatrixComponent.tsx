317d8d5b133119935c7c44ba23701603ba623c6c
roivasxxx <tiragoshi@gmail.com>
2022-12-16 17:42:42 +0100

    matrix state

--- src/components/MatrixComponent.tsx
+++ src/components/MatrixComponent.tsx
@@ -2,6 +2,15 @@ import { createSignal } from "solid-js";
 
 const MatrixComponent = () => {
   const [state, setState] = createSignal({ n: 1, matrix: [] });
+
+  const stateHandler=(value:string)=>
+  {
+    console.debug(Number.isNaN(Number.parseInt(value)))
+    const numberValue:number=Number.isNaN(Number.parseInt(value))?1:Number.parseInt(value)
+    setState({...state(),n:numberValue});      
+
+  }
+
   return (
     <div>
       <input
@@ -9,6 +18,8 @@ const MatrixComponent = () => {
         min={1}
         placeholder="Zadejte počet vrcholů"
         class=""
+        value={state().n}
+        onInput={(e)=>stateHandler(e.currentTarget.value)}
       />
     </div>
   );
